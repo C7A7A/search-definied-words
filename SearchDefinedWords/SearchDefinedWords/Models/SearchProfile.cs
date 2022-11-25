ced8213f7cf40f7c187a1f775afb50da0bc9fedd
Mateusz <czajkamateusz999@gmail.com>
2022-11-25 17:43:34 +0100

    add searchProfile model

---
+++ SearchDefinedWords/SearchDefinedWords/Models/SearchProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace SearchDefinedWords.Models {
+    public class SearchProfile {
+        private long Id { get; set; }
+
+        private List<string> Words { get; set; }
+
+        public SearchProfile(long id, List<string> words) {
+            this.Id = id;
+            this.Words = words;
+        }
+
+        public void AddWords(List<string> words) {
+            this.Words.AddRange(words);
+        }
+    }
+}
